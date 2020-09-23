535c1ea19016e598de54f3eecb5ee88cfd2db21e
simplex06 <ozkanbali.76@gmail.com>
2020-09-23 14:16:45 +0200

    roman v2

--- ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app2.py
+++ ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app2.py
@@ -2,24 +2,35 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def roman(number):
+    number = int(number)
+    integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_num = ''
+    x = 0
+    while  number > 0:
+        for i in range(number // integer[x]):
+            roman_num += romans[x]
+            number -= integer[x]
+        x += 1
+    return roman_num
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('index.html', developer_name= 'Ozkan', not_valid = False )
+
+
 @app.route("/", methods = ["GET", "POST"])
 def head():
-    if request.method == "POST":
-        num = request.form["number"]
-        if number.isdecimal():
-            number = int(number)
-            integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-            romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-            roman_num = ''
-            x = 0
-            while  number > 0:
-                for i in range(number // integer[x]):
-                    roman_num += romans[x]
-                    number -= integer[x]
-                x += 1
-        return render_template("result.html", number = num, developer_name="E2105-Ozkan")
-    else:
-        return render_template("index.html", developer_name="E2105-Ozkan", not_valid = True)
+    sayi = request.form['number']
+    
+    if not sayi.isdecimal():
+        return render_template('index.html', developer_name ='Ozkan', not_valid = True)
+    sayi = int(sayi)
+    if not 0 < sayi< 4000 :
+        return render_template('index.html', developer_name ='Ozkan', not_valid = True)
+    
+    return render_template("result.html", number_decimal = sayi, number_roman = roman(sayi) , developer_name="E2105-Ozkan")
     #return render_template("index.html", developer_name="E2105-Ozkan")
 
 if __name__ == "__main__":
