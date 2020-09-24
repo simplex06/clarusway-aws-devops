605c05d5c5d2b2791d3c47280985397621ea5455
simplex06 <ozkanbali.76@gmail.com>
2020-09-24 14:05:33 +0200

    roman update

--- ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app.py
+++ ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app.py
@@ -7,7 +7,7 @@ def head():
     if request.method == "POST":
         num = request.form["number"]
         if number.isdecimal():
-            number = int(number)
+            number = int(num)
             integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
             romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
             roman_num = ''
@@ -17,10 +17,10 @@ def head():
                     roman_num += romans[x]
                     number -= integer[x]
                 x += 1
-        return render_template("result.html", number = num, developer_name="E2105-Ozkan", not_valid = False)
+        return render_template("result.html", number_decimal = num, developer_name="E2105-Ozkan", not_valid = False)
     else:
         return render_template("index.html", developer_name="E2105-Ozkan", not_valid = True)
-    #return render_template("index.html", developer_name="E2105-Ozkan")
+    
 
 if __name__ == "__main__":
     #app.run(debug = True)
