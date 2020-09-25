fb2756c4a86006f52c4377dc84787a34ae021abf
simplex06 <ozkanbali.76@gmail.com>
2020-09-25 12:58:18 +0200

    roman numeral convertor update

--- ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app.py
+++ ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app.py
@@ -2,24 +2,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('index.html', developer_name= 'E2105-Ozkan', not_valid = False )
+
 @app.route("/", methods = ["GET", "POST"])
 def head():
     if request.method == "POST":
         num = request.form["number"]
-        if number.isdecimal():
-            number = int(num)
-            integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-            romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-            roman_num = ''
-            x = 0
-            while  number > 0:
-                for i in range(number // integer[x]):
-                    roman_num += romans[x]
-                    number -= integer[x]
-                x += 1
-        return render_template("result.html", number_decimal = num, developer_name="E2105-Ozkan", not_valid = False)
-    
-    return render_template("index.html", developer_name="E2105-Ozkan", not_valid = True)
+
+        if not num.isdecimal():
+            return render_template("index.html", developer_name="E2105-Ozkan", not_valid = True)
+        
+        number = int(num)
+        if not (0 < number < 4000):
+            return render_template("index.html", developer_name="E2105-Ozkan", not_valid = True)
+
+        integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        roman_num = ''
+        x = 0
+        while  number > 0:
+            for i in range(number // integer[x]):
+                roman_num += romans[x]
+                number -= integer[x]
+            x += 1
+        return render_template("result.html", number_decimal = num, number_roman = roman_num, developer_name="E2105-Ozkan", not_valid = True)
     
 
 if __name__ == "__main__":
--- ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app2.py
+++ ozkan-aws-devops/aws/projects/001-roman-numerals-converter/app2.py
@@ -33,6 +33,6 @@ def head():
     return render_template("result.html", number_decimal = sayi, number_roman = roman(sayi) , developer_name="E2105-Ozkan")
 
 if __name__ == "__main__":
-    #app.run(debug = True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug = True)
+    #app.run(host='0.0.0.0', port=80)
 
