d5943fc766ceb961883dab4a8f12d99d97a2ec29
simplex06 <ozkanbali.76@gmail.com>
2020-10-27 17:07:55 +0100

    new update

---
+++ ozkan-aws-devops/python/coding-challenges/cc-008-find-non-repeated-values/non-repeated-values_v2.py
@@ -0,0 +1,11 @@
+products = ["Pride and Prejudice", "To Kill a Mockingbird", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice", "In Cold Blood", "Wide Sargasso Sea",\
+"One Hundred Years of Solitude", "Brave New World",  "The Great Gatsby", "Brave New World",\
+"I Capture The Castle", "Brave New World", "The Great Gatsby", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice"]
+
+dic = {i: products.count(i) for i in products}
+
+for key, value in dic.items():
+    if dic[key] == 1:
+        print(key)
